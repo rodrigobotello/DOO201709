8ba8b5ddff0ec0f5673c5f2ec7ea0ccd83b12fec
Rodrigo Botello <armandorodrigoba@gmail.com>
2017-04-23 23:18:18 -0500

    Add files via upload

---
+++ Labs/Laboratorio 10/Lab10/src/lab10/Shape.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab10;
+
+/**
+ *
+ * @author arman
+ */
+
+    public interface Shape {
+    void draw();
+    }
+    
---
+++ Labs/Laboratorio 10/Lab10/src/lab10/ShapeFactory.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab10;
+
+
+/**
+ *
+ * @author arman
+ */
+public class ShapeFactory {
+
+   public Shape getShape(String shapeType){
+      if(shapeType == null){
+         return null;
+      }
+      if(shapeType.equals("CIRCLE")){
+         return new Circle();
+
+      } else if(shapeType.equals("RECTANGLE")){
+         return new Rectangle();
+
+      } else if(shapeType.equals("SQUARE")){
+         return new Square();
+      }
+
+      return null;
+   }
+}
